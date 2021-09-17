d611b31adc3e78692c93327391734c28121ba942
Dmitry Kankalovich <dmitry.kankalovich@gmail.com>
2021-09-17 11:11:58 +0200

    Verify signing chain fix (#306)
    * Fix verifySigningChain so that openssl verify method correctly used
    
    * Updated tests for verifySigningChain
    
    * Added specific tests to verify same Root CA trust
    
    Co-authored-by: Dmitry Kankalovich <konkolovich.dmitry@gmail.com>

--- test/pem.spec.js
+++ test/pem.spec.js
@@ -714,6 +714,11 @@ describe('General Tests', function () {
           commonName: 'Intermediate CA Certificate',
           serviceKey: ca.serviceKey,
           serviceCertificate: ca.certificate,
+          config:
+              `
+              [v3_req]
+              basicConstraints = critical,CA:TRUE
+              `,
           serial: Date.now()
         }, function (error, intermediate) {
           hlp.checkError(error)
@@ -731,7 +736,7 @@ describe('General Tests', function () {
             hlp.checkTmpEmpty()
 
             // chain check ok
-            pem.verifySigningChain([intermediate.certificate, cert.certificate], [
+            pem.verifySigningChain([cert.certificate, intermediate.certificate, ca.certificate], [
               ca.certificate, intermediate.certificate
             ], function (error, valid) {
               hlp.checkError(error)
@@ -756,6 +761,81 @@ describe('General Tests', function () {
                     done()
                   })
               })
+            })
+          })
+        })
+      })
+      it('Verify same Root CA trust', function (done) {
+        // Verify TLS cert [Leaf, Int CA 2, Root CA] is valid
+        // to the trust store [Int CA 1, Root CA] since it has
+        // the same Root CA in the trust chain
+        pem.createCertificate({
+          commonName: 'Intermediate CA 1 Certificate',
+          serviceKey: ca.serviceKey,
+          serviceCertificate: ca.certificate,
+          config:
+              `
+              [v3_req]
+              basicConstraints = critical,CA:TRUE
+              `,
+          serial: Date.now()
+        }, function (error, intCa1) {
+          hlp.checkError(error)
+          hlp.checkCertificate(intCa1)
+          hlp.checkTmpEmpty()
+
+          pem.createCertificate({
+            commonName: 'Intermediate CA 2 Certificate',
+            serviceKey: ca.serviceKey,
+            serviceCertificate: ca.certificate,
+            config:
+                `
+                [v3_req]
+                basicConstraints = critical,CA:TRUE
+                `,
+            serial: Date.now()
+          }, function (error, intCa2) {
+            hlp.checkError(error)
+            hlp.checkCertificate(intCa2)
+            hlp.checkTmpEmpty()
+
+            pem.createCertificate({
+              serviceKey: intCa2.clientKey,
+              serviceCertificate: intCa2.certificate,
+              serial: Date.now()
+            }, function (error, cert) {
+              hlp.checkError(error)
+              hlp.checkCertificate(cert)
+              hlp.checkTmpEmpty()
+
+              // chain check ok
+              pem.verifySigningChain(
+                [cert.certificate, intCa2.certificate, ca.certificate],
+                [ca.certificate, intCa1.certificate],
+                function (error, valid) {
+                  hlp.checkError(error)
+                  expect(valid).to.be.true()
+
+                  // chain check fails -> missing ca cert in chain
+                  pem.verifySigningChain(
+                    [cert.certificate, intCa2.certificate],
+                    [intCa1.certificate],
+                    function (error, valid) {
+                      hlp.checkError(error)
+                      expect(valid).to.be.false()
+
+                      // chain check fails -> missing intermediate cert in chain
+                      pem.verifySigningChain(
+                        cert.certificate, [
+                          ca.certificate
+                        ],
+                        function (error, valid) {
+                          hlp.checkError(error)
+                          expect(valid).to.be.false()
+                          done()
+                        })
+                    })
+                })
             })
           })
         })
