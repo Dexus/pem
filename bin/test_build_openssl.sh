ce1d8e4573ebbf2a5fff5bf0c5c5a38f0b25a290
Josef Fröhle <josef.froehle@greenbone.net>
2022-01-21 23:20:56 +0100

    don't check for openssl bin from cache

--- bin/test_build_openssl.sh
+++ bin/test_build_openssl.sh
@@ -17,74 +17,71 @@ checkDebInstalled() {
   fi
 }
 
-if [[ ! -f "${OPENSSL_DIR}/bin/openssl" ]]; then
+for pkg in "curl" "build-essential" "ca-certificates" "ca-certificates-java" "gnutls-bin" "libnss3-tools" "ca-certificates-mono" "checkinstall" "zlib1g-dev" "libtemplate-perl"; do
+  checkDebInstalled "$pkg"
+done
 
-  for pkg in "curl" "build-essential" "ca-certificates" "ca-certificates-java" "gnutls-bin" "libnss3-tools" "ca-certificates-mono" "checkinstall" "zlib1g-dev" "libtemplate-perl"; do
-    checkDebInstalled "$pkg"
-  done
+case "${LIBRARY}" in
+"libressl")
+  URL1="http://ftp.eu.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${VERSION}.tar.gz"
+  URL2="http://ftp3.usa.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${VERSION}.tar.gz"
+  ;;
+"openssl")
+  URL1="https://openssl.org/source/openssl-${VERSION}.tar.gz"
+  URL2="http://mirrors.ibiblio.org/openssl/source/openssl-${VERSION}.tar.gz"
+  ;;
+esac
 
-  case "${LIBRARY}" in
-  "libressl")
-    URL1="http://ftp.eu.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${VERSION}.tar.gz"
-    URL2="http://ftp3.usa.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${VERSION}.tar.gz"
-    ;;
-  "openssl")
-    URL1="https://openssl.org/source/openssl-${VERSION}.tar.gz"
-    URL2="http://mirrors.ibiblio.org/openssl/source/openssl-${VERSION}.tar.gz"
-    ;;
-  esac
+case "${TARGET}" in
+"x86_64-unknown-linux-gnu")
+  OS_COMPILER=linux-x86_64
+  ;;
+"i686-unknown-linux-gnu")
+  OS_COMPILER=linux-elf
+  OS_FLAGS=-m32
+  ;;
+"arm-unknown-linux-gnueabihf")
+  OS_COMPILER=linux-armv4
+  export AR=arm-linux-gnueabihf-ar
+  export CC=arm-linux-gnueabihf-gcc
+  ;;
+esac
 
-  case "${TARGET}" in
-  "x86_64-unknown-linux-gnu")
-    OS_COMPILER=linux-x86_64
-    ;;
-  "i686-unknown-linux-gnu")
-    OS_COMPILER=linux-elf
-    OS_FLAGS=-m32
-    ;;
-  "arm-unknown-linux-gnueabihf")
-    OS_COMPILER=linux-armv4
-    export AR=arm-linux-gnueabihf-ar
-    export CC=arm-linux-gnueabihf-gcc
-    ;;
-  esac
+mkdir -p /tmp/build
+cd /tmp/build
 
-  mkdir -p /tmp/build
-  cd /tmp/build
-
-  OUT=/tmp/openssl.tgz
-  MAX_REDIRECTS=5
-  if [[ ! -f "${OUT}" ]]; then
-    curl -o ${OUT} -L --max-redirs ${MAX_REDIRECTS} "${URL1}" ||
-      curl -o ${OUT} -L --max-redirs ${MAX_REDIRECTS} "${URL2}"
-    tar --strip-components=1 -xzf ${OUT}
-  fi
-
-  case "${LIBRARY}" in
-  "openssl")
-    ./Configure --prefix="${OPENSSL_DIR}" ${OS_COMPILER} -fPIC -g ${OS_FLAGS} no-shared -static
-    ;;
-  "libressl")
-    ./configure --prefix="${OPENSSL_DIR}" --disable-shared --with-pic
-    ;;
-  esac
-
-  make -s -j$(nproc)
-  make -s install_sw
-
-  case "${LIBRARY}" in
-  "openssl")
-    if [[ ! -f "${OPENSSL_DIR}/ssl/openssl.cnf" ]]; then mkdir -p "${OPENSSL_DIR}/ssl" && cp apps/openssl.cnf "${OPENSSL_DIR}/ssl/openssl.cnf"; fi
-    ;;
-  "libressl")
-    if [[ ! -f "${OPENSSL_DIR}/ssl/openssl.cnf" ]] && [[ -f "apps/openssl/openssl.cnf" ]]; then mkdir -p "${OPENSSL_DIR}/ssl" && cp apps/openssl/openssl.cnf "${OPENSSL_DIR}/ssl/openssl.cnf"; fi
-    ;;
-  esac
-
-  sudo update-ca-certificates || true
-  sudo cp -a /etc/ssl/certs/. "${OPENSSL_DIR}/ssl/certs/"
-  sudo chown -R $(id -u):$(id -g) "${OPENSSL_DIR}"
-  sudo chmod -Rf 0755 /openssl
+OUT=/tmp/openssl.tgz
+MAX_REDIRECTS=5
+if [[ ! -f "${OUT}" ]]; then
+  curl -o ${OUT} -L --max-redirs ${MAX_REDIRECTS} "${URL1}" ||
+    curl -o ${OUT} -L --max-redirs ${MAX_REDIRECTS} "${URL2}"
+  tar --strip-components=1 -xzf ${OUT}
 fi
+
+case "${LIBRARY}" in
+"openssl")
+  ./Configure --prefix="${OPENSSL_DIR}" ${OS_COMPILER} -fPIC -g ${OS_FLAGS} no-shared -static
+  ;;
+"libressl")
+  ./configure --prefix="${OPENSSL_DIR}" --disable-shared --with-pic
+  ;;
+esac
+
+make -s -j$(nproc)
+make -s install_sw
+
+case "${LIBRARY}" in
+"openssl")
+  if [[ ! -f "${OPENSSL_DIR}/ssl/openssl.cnf" ]]; then mkdir -p "${OPENSSL_DIR}/ssl" && cp apps/openssl.cnf "${OPENSSL_DIR}/ssl/openssl.cnf"; fi
+  ;;
+"libressl")
+  if [[ ! -f "${OPENSSL_DIR}/ssl/openssl.cnf" ]] && [[ -f "apps/openssl/openssl.cnf" ]]; then mkdir -p "${OPENSSL_DIR}/ssl" && cp apps/openssl/openssl.cnf "${OPENSSL_DIR}/ssl/openssl.cnf"; fi
+  ;;
+esac
+
+sudo update-ca-certificates || true
+sudo cp -a /etc/ssl/certs/. "${OPENSSL_DIR}/ssl/certs/"
+sudo chown -R $(id -u):$(id -g) "${OPENSSL_DIR}"
+sudo chmod -Rf 0755 /openssl
 
 cd "${NORMALPATH}"
